8a159fdf7411949d4738710c07bcc11dcfd3dbf3
Deepanshu saini <deepanshusaini2711@gmail.com>
2024-08-14 17:00:17 +0530

    added loading.tsx

---
+++ app/loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="md:text-7xl text-4xl font-bold">SAVE-TO-DRIVE</h1>
+    </div>
+  );
+};
+
+export default Loading;
